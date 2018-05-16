c26715b6470c32727446c79fb77887cdab9d1c26
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2018-05-16 16:56:44 +1000

    needs to be recurse for recursive

--- recursor/entrypoint.sh
+++ recursor/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 export PORT=${PORT:-"53"}
-export FORWARD_ZONES=${FORWARD_ZONES:-".=9.9.9.9,.=149.112.112.112"}
+export FORWARD_ZONES_RECURSE=${FORWARD_ZONES_RECURSE:-".=9.9.9.9,.=149.112.112.112"}
+export ALLOW_FROM=${ALLOW_FROM:-"127.0.0.0/8, 10.0.0.0/8, 100.64.0.0/10, 169.254.0.0/16, 192.168.0.0/16, 172.16.0.0/12, ::1/128, fc00::/7, fe80::/10"}
 
 sed -i "s|local-port=5353|local-port=$PORT|g" /etc/pdns/recursor.conf
-sed -i "s|# forward-zones=|forward-zones=$FORWARD_ZONES|g" /etc/pdns/recursor.conf
+sed -i "s|# forward-zones-recurse=|forward-zones-recurse=$FORWARD_ZONES_RECURSE|g" /etc/pdns/recursor.conf
+sed -i "s|# allow-from=127.0.0.0/8, 10.0.0.0/8, 100.64.0.0/10, 169.254.0.0/16, 192.168.0.0/16, 172.16.0.0/12, ::1/128, fc00::/7, fe80::/10|allow-from=$ALLOW_FROM|g" /etc/pdns/recursor.conf
 
 /usr/sbin/pdns_recursor
