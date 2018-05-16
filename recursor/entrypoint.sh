4cbff9feebb0036a1f7f520f90120b6a5915f7ec
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2018-05-16 15:23:16 +1000

    still wrong

--- recursor/entrypoint.sh
+++ recursor/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PORT=${PORT:-"53"}
-export FORWARD_ZONES=${FORWARD_ZONES:-".=9.9.9.9,149;112.112.112"}
+export FORWARD_ZONES=${FORWARD_ZONES:-".=9.9.9.9,.=149.112.112.112"}
 
 sed -i "s|local-port=5353|local-port=$PORT|g" /etc/pdns/recursor.conf
 sed -i "s|# forward-zones=|forward-zones=$FORWARD_ZONES|g" /etc/pdns/recursor.conf
